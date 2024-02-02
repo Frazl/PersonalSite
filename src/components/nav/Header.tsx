b34af0f65fd1a1093e4f72fdf27214b5bd51844e
Fraz <sean.fradl@gmail.com>
2024-02-02 10:13:33 +0000

    Update heading styling to swap to a column layout on smaller devices

--- src/components/nav/Header.tsx
+++ src/components/nav/Header.tsx
@@ -5,12 +5,13 @@ const headerStyle = {
     minHeight: '2em',
     width: '100%',
     display: 'flex',
-    marginTop: '1em',
-    marginBottom: '1.5em',
+    paddingTop: '1em',
+    paddingBottom: '1.5em',
     justifyContent: 'space-around',
 }
 
-const refStyle: React.CSSProperties = {
+const menuItemStyle: React.CSSProperties = {
+  marginTop: '.5em',
   fontSize: '1.4em',
   display: 'flex',
   justifyContent: 'space-around'
@@ -24,10 +25,10 @@ function Header(props: HeaderProps) {
   return (
     <div style={{display: 'flex', flexDirection: 'column'}}>
       <img src="./SeanFradlLogoWhite.png" alt="Sean Fradl Logo" style={{margin: 'auto'}} />
-      <div style={headerStyle} className="header">
-        <a href="#resume" onClick={() => props.setActiveApp('resume')} style={refStyle}>Resume & Generator</a>
-        <a href="#resume" onClick={() => props.setActiveApp('resume')} style={refStyle}>Home</a>
-        <a href="#rankedTable" onClick={() => props.setActiveApp('rankedTable')} style={refStyle}>Animated Ranked Table</a>
+      <div style={headerStyle} className="header smartDirection">
+        <a href="#resume" onClick={() => props.setActiveApp('resume')} style={menuItemStyle}>Resume & Generator</a>
+        <a href="#resume" onClick={() => props.setActiveApp('resume')} style={menuItemStyle}>Home</a>
+        <a href="#rankedTable" onClick={() => props.setActiveApp('rankedTable')} style={menuItemStyle}>Animated Ranked Table</a>
       </div>
     </div>
   );
